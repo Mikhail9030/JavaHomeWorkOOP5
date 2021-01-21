30d1fba1d1c8f07457bd20430a1ce18ae5792712
Mikhail9030 <gulaevmihail112@gmail.com>
2021-01-21 23:26:58 +0300

    Commit 2

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -13,11 +13,11 @@ class ProductManagerTest {
     private ProductRepository repository = new ProductRepository();
     private ProductManager manager = new ProductManager(repository);
 
-    private Product book1 = new Book(1, "День", 99999, "Валерий");
-    private Product book2 = new Book(13, "Ночь", 1990, "Василий");
-    private Product phone1 = new Smartphone(99, "One Action", 1550000, "Motorola");
-    private Product phone2 = new Smartphone(7, "Galaxy A10", 793000, "Samsung");
-    private Product phone3 = new Smartphone(2, "G7 Plus", 1139000, "Motorola");
+    private Product book1 = new Book(1, "День", 99999, "Виктор");
+    private Product book2 = new Book(13, "Ночь", 1990, "Алексей");
+    private Product phone1 = new Smartphone(99, "One Action", 1550000, "Iphone");
+    private Product phone2 = new Smartphone(7, "Galaxy S20", 793000, "Samsung");
+    private Product phone3 = new Smartphone(2, "G7 Plus", 1139000, "Iphone");
     private Product product1 = new Product(5, "None", 10);
 
     @BeforeEach
@@ -40,7 +40,7 @@ class ProductManagerTest {
 
     @Test
     void searchByAuthorBookOneFoundTest() {
-        Product[] actual = manager.searchBy("василий");
+        Product[] actual = manager.searchBy("алексей");
         Product[] expected = new Product[]{book2};
 
         assertArrayEquals(expected, actual);
@@ -56,7 +56,7 @@ class ProductManagerTest {
 
     @Test
     void searchByNamePhoneOneFoundTest() {
-        Product[] actual = manager.searchBy("gaLaxy a10");
+        Product[] actual = manager.searchBy("galaxy S20");
         Product[] expected = new Product[]{phone2};
 
         assertArrayEquals(expected, actual);
@@ -64,7 +64,7 @@ class ProductManagerTest {
 
     @Test
     void searchByVendorPhoneSeveralFoundTest() {
-        Product[] actual = manager.searchBy("motoroLa");
+        Product[] actual = manager.searchBy("iphone");
         Product[] expected = new Product[]{phone1, phone3};
 
         assertArrayEquals(expected, actual);
